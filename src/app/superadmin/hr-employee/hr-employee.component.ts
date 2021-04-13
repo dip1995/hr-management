8ab041940f926caafacf3bbe8eb00ea7a523bf8c
dip1995 <63094661+dip1995@users.noreply.github.com>
2021-04-13 18:31:34 +0530

    13-04-2021
    13-04-2021

--- src/app/superadmin/hr-employee/hr-employee.component.ts
+++ src/app/superadmin/hr-employee/hr-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 @Component({
   selector: 'app-hr-employee',
@@ -6,13 +9,126 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hr-employee.component.css']
 })
 export class HrEmployeeComponent implements OnInit {
-  addUpdateEmployee:boolean = true;
-  constructor() { }
+  addUpdateEmployee:boolean = false;
+   session:boolean = false;
+   gridApi;
+   gridColumnApi;
+
+   columnDefs;
+   defaultColDef;
+   defaultColGroupDef;
+   columnTypes;
+   rowData: [];
+   dateParts;
+   day;
+   month;
+   year;
+   cellDate;
+
+  constructor(private router : Router) {
+    this.columnDefs = [
+      {
+        headerName: 'Name',
+        field: 'Name',
+      },
+      {
+        headerName: 'Mobile',
+        field: 'Mobile',
+        type: 'numberColumn',
+      },
+      {
+        headerName: 'Email',
+        field: 'Email',
+        
+      },
+      {
+        headerName: 'Salary',
+        field: 'Salary',
+        type: 'numberColumn',
+      },
+      {
+        headerName: 'Leave Credit',
+        field: 'Leave Credit',
+        type: 'numberColumn',
+      },
+      {
+        headerName: 'Joining Date',
+        field: 'Joining Date',
+        type: ['dateColumn', 'nonEditableColumn'],
+        width: 220,
+      },
+      {
+        headerName: 'Increment Date',
+        field: 'Increment Date',
+        type: ['dateColumn', 'nonEditableColumn'],
+        width: 220,
+      },
+      {
+        headerName: 'Document Upload',
+        field: 'Document Upload',
+        // type: 'file',
+      },
+
+      ]
+
+    this.defaultColDef = {
+      width: 150,
+      editable: true,
+      filter: 'agTextColumnFilter',
+      floatingFilter: true,
+      resizable: true,
+    };
+    this.defaultColGroupDef = { marryChildren: true };
+    this.columnTypes = {
+      numberColumn: {
+        width: 130,
+        filter: 'agNumberColumnFilter',
+      },
+      nonEditableColumn: { editable: false },
+      dateColumn: {
+        filter: 'agDateColumnFilter',
+        filterParams: {
+          comparator: function (filterLocalDateAtMidnight, cellValue) {
+             this.dateParts = cellValue.split('/');
+             this.day = Number(this.dateParts[0]);
+             this.month = Number(this.dateParts[1]) - 1;
+             this.year = Number(this.dateParts[2]);
+             this.cellDate = new Date(this.year, this.month, this.day);
+            if (this.cellDate < filterLocalDateAtMidnight) {
+              return -1;
+            } else if (this.cellDate > filterLocalDateAtMidnight) {
+              return 1;
+            } else {
+              return 0;
+            }
+          },
+        },
+      },
+    };
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+
+  }
 
   ngOnInit(): void {
   }
 
+  addEmployee(){
+    this.addUpdateEmployee = true;
+  }
+
   addUpdateEmployeeDetails(){
+  }
+
+  endSession(){
+   this.session = true;
+  }
+
+  onDelete(){
 
   }
 
--- src/app/superadmin/hr-leave-application/hr-leave-application.component.ts
+++ src/app/superadmin/hr-leave-application/hr-leave-application.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 @Component({
   selector: 'app-hr-leave-application',
@@ -7,42 +9,97 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./hr-leave-application.component.css']
 })
 export class HrLeaveApplicationComponent implements OnInit {
-  data;
-  selectMonth;
- dateFromDisable;
- dateToDisable;
- todayDate = new Date();
 
- constructor() { }
+  gridApi;
+  gridColumnApi;
 
-  ngOnInit(): void {
+  columnDefs;
+  defaultColDef;
+  defaultColGroupDef;
+  columnTypes;
+  rowData: [];
+  dateParts;
+  day;
+  month;
+  year;
+  cellDate;
+
+ constructor(private router : Router) {
+   this.columnDefs = [
+     {
+       headerName: 'Name',
+       field: 'Name',
+     },
+     {
+       headerName: 'Date From',
+       field: 'Date From',
+       type: ['dateColumn', 'nonEditableColumn'],
+       width: 220,
+     },
+     {
+       headerName: 'Date To',
+       field: 'Date To',
+       type: ['dateColumn', 'nonEditableColumn'],
+       width: 220,
+     },
+     {
+       headerName: 'Description',
+       field: 'Description',
+      //  type: 'file',
+     },
+     {
+      headerName: 'Total Leave',
+      field: 'Total Leave',
+      type: 'numberType',
+    },
+
+     ]
+
+   this.defaultColDef = {
+     width: 150,
+     editable: true,
+     filter: 'agTextColumnFilter',
+     floatingFilter: true,
+     resizable: true,
+   };
+   this.defaultColGroupDef = { marryChildren: true };
+   this.columnTypes = {
+     numberColumn: {
+       width: 130,
+       filter: 'agNumberColumnFilter',
+     },
+     nonEditableColumn: { editable: false },
+     dateColumn: {
+       filter: 'agDateColumnFilter',
+       filterParams: {
+         comparator: function (filterLocalDateAtMidnight, cellValue) {
+            this.dateParts = cellValue.split('/');
+            this.day = Number(this.dateParts[0]);
+            this.month = Number(this.dateParts[1]) - 1;
+            this.year = Number(this.dateParts[2]);
+            this.cellDate = new Date(this.year, this.month, this.day);
+           if (this.cellDate < filterLocalDateAtMidnight) {
+             return -1;
+           } else if (this.cellDate > filterLocalDateAtMidnight) {
+             return 1;
+           } else {
+             return 0;
+           }
+         },
+       },
+     },
+   };
+ }
+
+ onGridReady(params) {
+   this.gridApi = params.api;
+   this.gridColumnApi = params.columnApi;
+
+
+ }
+
+  ngOnInit(){
 
   }
-
-  dateFrom(){
-    let year =  this.todayDate.getFullYear();
-    let month = (this.todayDate.getMonth() > 9 ? this.todayDate.getMonth()+1 : "0"+(this.todayDate.getMonth()+1));
-    let day = (this.todayDate.getDate() > 9 ? this.todayDate.getDate() : "0"+(this.todayDate.getDate()));
-    this.dateFromDisable = year + "-" + month + "-" + day;
-    console.log(this.dateFromDisable);
-
-    return this.dateFromDisable;
-
-
-  }
-
-  toFrom(){
-    this.dateToDisable = this.todayDate;
-  }
-
-
-  submitLeaveApplication(obj){
-
-    this.data = obj;
-    console.log(obj);
-  }
-
-  onCancel(f: NgForm){
-   f.form.reset();
-  }
+ 
 }
