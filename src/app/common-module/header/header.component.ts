c163fd78fff802237fdefe72cbbd6f234db7132d
epmanglesh <manglesh.p@engineerphilosophy.com>
2021-04-20 22:21:07 +0530

    new changes
    new changes

--- src/app/common-module/header/header.component.ts
+++ src/app/common-module/header/header.component.ts
@@ -14,23 +14,26 @@ export class HeaderComponent implements OnInit {
 
   linkUrl:any = environment.LINK_URL; // url
   userCookie:any;
-
+  shortname:any = "";
   constructor(
     private router: Router,
     private cookieService : CookieService,
     private employeeService : EmployeeService
   ) {
-    this.userCookie = this.cookieService.get('user') ? JSON.parse(this.cookieService.get('user')) : {};
+    this.userCookie = this.cookieService.get('epuser') ? JSON.parse(this.cookieService.get('epuser')) : {};
   }
 
   ngOnInit(): void {
-
+    let name = this.userCookie.name ? this.userCookie.name.split(" ") : [];
+    let first_name = name.length > 0 && name[0] ? name[0] : "";
+    let last_name = name.length > 0 && name[1] ? name[1] : "";
+    this.shortname = this.firstLetter(first_name,last_name);
   }
 
   logoutEmployee() {
     this.employeeService.employeeLogout({}).subscribe(res=> {
-      this.cookieService.delete('user',  ' / ' ,  'localhost');
-      this.cookieService.delete('user');
+      this.cookieService.delete('epuser',  ' / ' ,  'localhost');
+      this.cookieService.delete('epuser');
       this.router.navigate(['/login']);
         // this.userinfoService.clearTimer(obj).subscribe(res=> {
         // });
@@ -39,5 +42,14 @@ export class HeaderComponent implements OnInit {
 
   changePassword(){
     this.router.navigate(['change-password']);
+  }
+
+  firstLetter(fst, lst) {
+    if (fst) {
+      if (lst) {
+          return fst.substr(0, 1).toUpperCase() + lst.substr(0, 1).toUpperCase(); // abc -> A
+      }else
+      return fst.substr(0, 1).toUpperCase();
+    }
   }
 }
